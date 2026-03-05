748ec8a703e86c813cd6ccff47204454c8592b22
wanderingsunbi <neoguysm@gmail.com>
2026-03-05 18:57:25 +0900

    ui 색상 적용

--- src/components/ui/dropdown.stories.tsx
+++ src/components/ui/dropdown.stories.tsx
@@ -20,12 +20,16 @@ export const Default: Story = {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="outline" className="group flex w-[200px] items-center justify-between">
-            <span>{selected}</span>
-            <ChevronDown className="size-4 opacity-50 transition-transform duration-100 group-data-[state=open]:rotate-180" />
+          <Button 
+            variant="outline" 
+            className="group flex h-12 w-[260px] items-center justify-between border-[#F97B22] border-[1px] rounded-[16px] px-4 transition-all duration-200 hover:bg-orange-50/30" >
+            <span className={selected ? "text-[#F97B22] font-bold" : "text-[#94A3B8]"}>
+              {selected}
+            </span>
+            <ChevronDown className="size-4 opacity-50 text-[#F97B22] transition-transform duration-100 group-data-[state=open]:rotate-180" />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
+        <DropdownMenuContent className="w-[260px] rounded-[16px] border-[#F97B22]">
           <DropdownMenuItem onSelect={() => setSelected("청량/탄산음료")}>
             청량/탄산음료</DropdownMenuItem>
           <DropdownMenuItem onSelect={() => setSelected("과즙음료")}>
