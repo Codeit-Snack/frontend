7f38dad9f807b96244d8c0cdb930794ff2270047
hwang-inah <233007117+hwang-inah@users.noreply.github.com>
2026-03-19 08:10:57 +0900

    [fix] 구매 요청 날짜 포맷 수정

--- src/components/cart/item-request-modal.tsx
+++ src/components/cart/item-request-modal.tsx
@@ -44,7 +44,7 @@ export default function ItemRequestModal({
         year: "numeric",
         month: "2-digit",
         day: "2-digit",
-      }).replace(/\. /g, ".").replace(".", "."),
+      }).replace(/\. /g, ".").replace(/\.$/, ""),
       productSummary:
         items.length === 1
           ? items[0].name
